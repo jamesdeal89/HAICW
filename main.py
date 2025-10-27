29f0c74b77fbbb6fb837221dbc5c568c2549e9ea
James Deal <j891319@protonmail.com>
2025-10-27 16:26:46 +0000

    initial rules and ideas

---
+++ main.py
@@ -0,0 +1,30 @@
+
+# ====== COMP3074 Coursework Chatbot ======
+# Author: James Deal
+# ID: 20551937
+
+'''
+NOTE: Allowed libraries include: 
+- Python standard library.
+- numpy,
+- scipy,
+- scikit-learn (and it's dependencies),
+- nltk (EXCEPT nltk.chat)
+'''
+
+'''
+CHECKLIST OF EXPECTED FEATURES:
+1. Intent matching. [ ]
+2. Identity matching. [ ]
+3. Transactions. [ ]
+4. Information retrieval & Question answering. [ ]
+5. Small talk. [ ]
+'''
+
+'''
+INITIAL IDEAS:
+Book ordering system.
+Transaction: ordering books, scheduling pickup/delivery.
+Q&A: book recomendations, location opening times.
+'''
+
