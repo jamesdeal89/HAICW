d07d9948441ddd6119b49f1027db9b3581d21e02
James Deal <j891319@protonmail.com>
2025-11-02 09:55:42 +0000

    TODOs

--- main.py
+++ main.py
@@ -28,6 +28,27 @@ Transaction: ordering books, scheduling pickup/delivery.
 Q&A: book recomendations, location opening times.
 '''
 
+'''
+FOR THE CHECKPOINT:
+Requirements:
+Implement *similarity-based* intent matching, 
+which routes the flow of the chatbot to functions for handling:
+- Small talk,
+- Discoverability (what can the bot do?),
+- Identity management,
+- Question answering.
+
+They will test with:
+Hi or Hello [greet the user and ask their name]
+What is my name [tell the user their name]
+How are you [answer]
+What can you do [answer]
+What are stocks and bonds [answer from QA dataset]
+
+NOTE: For purposes of checkpoint, need to use *stocks QA dataset* provided, 
+for final submission can change.
+'''
+
 def main():
     print("======== STARTING BOOKSTORE CHATBOT ========")
     # NOTE: below ASCII art was generated via https://patorjk.com/software/taag/
@@ -56,5 +77,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# TODO: similarity-based intent matching
+
+# TODO: small talk 
+
+# TODO: discoverability
+
+# TODO: user personalisation / memory
+
+# TODO: Q&A with stocks & bonds dataset provided for checkpoint
+
 
 
